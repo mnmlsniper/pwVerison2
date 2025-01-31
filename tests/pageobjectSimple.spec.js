c7f2eadfb0923b77180846afdaefd84ba20a982a
Your Name <you@example.com>
2025-01-31 21:15:19 +0300

    add allure

--- tests/pageobjectSimple.spec.js
+++ tests/pageobjectSimple.spec.js
@@ -1,28 +1,41 @@
+import * as allure from 'allure-js-commons';
 import { test, expect } from '@playwright/test';
-import { faker, ne } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import { MainPage } from '../src/pages/mainPage';
 import { RegisterPage } from '../src/pages/registerPage';
 import { YourfeedPage } from '../src/pages/yourfeedPage';
 
 const URL_UI = 'https://realworld.qa.guru/';
 
-test('Пользователь может авторизоваться используя логин пароль', async ({
-	page,
-}) => {
-	const user = {
-		email: faker.internet.email(),
-		password: faker.internet.password({ length: 10 }),
-		username: faker.person.firstName(),
-	};
+test.describe('Авторизация', () => {
+	test('Пользователь может авторизоваться используя логин пароль', async ({
+		page,
+	}) => {
+		await allure.epic('Авторизация');
+		await allure.feature('Авторизация пользователя');
+		await allure.story('Авторизация через пароль');
 
-	const mainPage = new MainPage(page);
-	const registerPage = new RegisterPage(page);
-	const yourfeedPage = new YourfeedPage(page);
+		//await allure.displayName('Test Authentication');
+		await allure.owner('Lyubov Danilova');
+		await allure.tags('Web interface', 'Authentication');
+		await allure.severity('blocker');
+		const user = {
+			email: faker.internet.email(),
+			password: faker.internet.password({ length: 10 }),
+			username: faker.person.firstName(),
+		};
 
-	await mainPage.open(URL_UI);
-	await mainPage.gotoRegister();
-	await registerPage.register(user.username, user.email, user.password);
+		const mainPage = new MainPage(page);
+		const registerPage = new RegisterPage(page);
+		const yourfeedPage = new YourfeedPage(page);
 
-	await expect(yourfeedPage.profileNameField).toBeVisible();
-	await expect(yourfeedPage.profileNameField).toContainText(user.username);
+		await mainPage.open(URL_UI);
+		await mainPage.gotoRegister();
+		await registerPage.register(user.username, user.email, user.password);
+
+		await test.step('Проверить, что отображается имя пользователя в профиле', async () => {
+			await expect(yourfeedPage.profileNameField).toBeVisible();
+			await expect(yourfeedPage.profileNameField).toContainText(user.username);
+		});
+	});
 });
